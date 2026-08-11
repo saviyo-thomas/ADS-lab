e804b8417e14639a1ebc57e4b501c85e84394868
saviyo-thomas <saviyothomasgmail.com>
2026-08-11 13:12:14 +0000

    trial 1

--- stll.c
+++ stll.c
@@ -6,8 +6,61 @@ typedef struct node{
 	struct node* next;
 }n;
 
-int main(){
-	n* head;
+void clrscr(){
+  printf("\e[1;1H\e[2J");
+  return;
+}
 
+n* createnode(int d){
+  n* nn=(n*)malloc(sizeof(n));
+  nn->id=d;
+  nn->next=NULL;
+  return nn;
+}
+void push(n** h);
+void pop(n** h);
+void peek(n** h);
+
+int main(){
+	n* head=NULL;
+
+  int c;
+  while(1){
+    clrscr();
+    printf("\n1.Push\n2.Pop\n3.Peek\n4.Exit");
+    scanf("%d",&c);
+
+    switch(c){
+      case 1:{push(&head); break;}
+      case 2:{pop(&head); break;}
+      case 3:{peek(&head); break;}
+      case 4:return;
+      default:{printf("\n Enter a valid input");}
+  }}
 	return 0;
+}
+
+void push (n** h){
+  int d;
+  printf("\nEnter data :");
+  scanf("%d",&d);
+  n* nn=createnode(d);
+  if(*h==NULL){*h=nn; return;}
+  else{nn->next=*h; *h=nn;}
+  return;
+}
+
+void pop (n** h){
+  if(*h=NULL){printf("!!Stack Underflow!!");}
+  else{printf("%d popped",h->d);}
+  n* t=*h;
+  h=h->next;
+  free(t);
+  return;
+}
+
+void peek(n** h){
+  if(*h=NULL){printf("!!Stack Underflow!!");}
+  else{printf("%d is at the top",h->d);}
+  return;
 }
