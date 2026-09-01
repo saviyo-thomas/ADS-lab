c8c88c3cc34ad8b90f657a73ab3900faa52f03dd
sav <xaviothomas@gmail.com>
2026-09-01 13:57:35 +0530

    queue array error check

--- qar.c
+++ qar.c
@@ -1,49 +1,56 @@
 #include<stdio.h>
+#include<stdbool.h>
+#define size 3
 
+int a[size],b=-1,f=-1;
 
-int a[5],b=0,f=0;
 void enq(int d);
 void dq();
-void vq();
-int cq();
+void count();
+void dis();
 
-
+bool isfull(){
+  return b==size-1;
+}
+bool isempty(){
+  return f==-1;
+}
 int main(){
   int ch,d;
   while(1){
-   printf("\n1.Enqueue\n2.Dequeue\n3.Traversal\n4.Count\n5.Exit\nEnter your choice :");
+   printf("\n1.Enqueue\n2.Dequeue\n3.print\n4.Count\n5.Exit\nEnter your choice :");
    scanf("%d",&ch);
    switch(ch){
      case 1:{printf("\nData :"); scanf("%d",&d); enq(d); break;}
      case 2:dq(); break;
-     case 3:printf("Queue has %d elements",cq()); break;
-     case 4:cq(); break;
+     case 3:dis(); break;
+     case 4:count(); break;
      case 5:return 0;
-     default:printf("Enter valid input");
+     default:printf("\nEnter valid input");
   }}
   return 0;
 }
 
 void enq(int d){
-  if((f<b)&&(b-f)==5){printf("Queue is full"); return;}
-  else if((f))
-  a[b]=d;
-  b=(b+1)%100;
+  if(isfull()){printf("\nQueue is full please wait till the queue is empty again"); return;}
+  else {
+    b++; a[b]=d;}
 }
 
 void dq(){
-  if(b==f){printf("Queue is empty"); return;}
-  printf("\n%d is dequeued",a[f]); f=(f+1)%100;
+  if(isempty()){printf("\nQueue is empty");return;}
+  else {f++; printf("\n%d is dequeued",a[f]);}
 }
 
-void vq(){
-  printf("\n");
-  for (int i=f; f!=b; f=(f+1)%100){
-    printf("%d,"a[i]);
-  }
+void count(){
+  int c;
+  c=(b-f);
+  printf("b:%df:%d",b,f);
+  printf("\nNumber of elements in queue: %d",c);
 }
 
-int cq(){
-  if(f==b)return 0;
-  else if(f<b)return(b-f);
-}
+void dis(){
+  if(isempty()){printf("\nQueue is empty");return;} 
+  for (int i=f; i<=b;++i){
+    printf("[%d] ",a[i]);
+}}
